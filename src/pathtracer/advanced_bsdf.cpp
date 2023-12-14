f66cb9ebad494d55a1d57ad6ccd1872392ec5f41
Adorable-Qin <fzh0424@outlook.com>
2023-12-15 03:18:44 +0800

    init

---
+++ src/pathtracer/advanced_bsdf.cpp
@@ -0,0 +1,267 @@
+#include "bsdf.h"
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+
+#include "application/visual_debugger.h"
+
+using std::max;
+using std::min;
+using std::swap;
+
+namespace CGL {
+    // helper function
+    Vector3D BSDF::wavelength_dependent_BSDF(double wavelength, Vector3D reflectance) {
+        double red_cof = 1.0 / (wavelength - 600);
+        double green_cof = 1.0 / (wavelength - 550);
+        double blue_cof = 1.0 / (wavelength - 450);
+        double N = red_cof + green_cof + blue_cof;
+        double albedo = (red_cof * reflectance[0] + green_cof * reflectance[1] + blue_cof * reflectance[2]) / N;
+        return albedo;
+    }
+
+// Mirror BSDF //
+
+    Vector3D MirrorBSDF::f(const Vector3D wo, const Vector3D wi, double wavelength) {
+        return Vector3D();
+    }
+
+    Vector3D MirrorBSDF::sample_f(const Vector3D wo, Vector3D *wi, double *pdf, double wavelength) {
+
+        // TODO Assignment 7: Part 1
+        // Implement MirrorBSDF
+
+        *pdf = 1.0;
+        reflect(wo, wi);
+        return reflectance / abs_cos_theta(*wi);
+    }
+
+    void MirrorBSDF::render_debugger_node() {
+        if (ImGui::TreeNode(this, "Mirror BSDF")) {
+            DragDouble3("Reflectance", &reflectance[0], 0.005);
+            ImGui::TreePop();
+        }
+    }
+
+// Microfacet BSDF //
+
+    double MicrofacetBSDF::G(const Vector3D wo, const Vector3D wi) {
+        return 1.0 / (1.0 + Lambda(wi) + Lambda(wo));
+    }
+
+    double MicrofacetBSDF::D(const Vector3D h) {
+        // TODO Assignment 7: Part 2
+        // Compute Beckmann normal distribution function (NDF) here.
+        // You will need the roughness alpha.
+        double thetah = getTheta(h.unit());
+        double cos_1 = cos(thetah);
+        double tan_thetah_2 = (1 - cos_1 * cos_1) / (cos_1 * cos_1);
+
+        double up = exp(-tan_thetah_2 / (alpha * alpha));
+        double down = PI * alpha * alpha * cos_1 * cos_1 * cos_1 * cos_1;
+
+        double ans = up / down;
+        return ans;
+    }
+
+    Vector3D MicrofacetBSDF::F(const Vector3D wi) {
+        // TODO Assignment 7: Part 2
+        // Compute Fresnel term for reflection on dielectric-conductor interface.
+        // You will need both eta and etaK, both of which are Vector3D.
+        Vector3D F;
+        double cosval = cos(getTheta(wi.unit()));
+
+        Vector3D Rs_up = eta * eta + k * k - 2 * eta * cosval + cosval * cosval;
+        Vector3D Rs_down = eta * eta + k * k + 2 * eta * cosval + cosval * cosval;
+        Vector3D Rs = Rs_up / Rs_down;
+
+        Vector3D Rp_up = (eta * eta + k * k) * cosval * cosval - 2 * eta * cosval + 1;
+        Vector3D Rp_down = (eta * eta + k * k) * cosval * cosval + 2 * eta * cosval + 1;
+        Vector3D Rp = Rp_up / Rp_down;
+
+        F = (Rs + Rp) / 2;
+        return F;
+    }
+
+    Vector3D MicrofacetBSDF::f(const Vector3D wo, const Vector3D wi, double wavelength) {
+        Vector3D ans(0.0, 0.0, 0.0);
+        if (wo.z >= 0) {
+            Vector3D h = ((wo + wi) / 2).unit();
+            Vector3D up = F(wi) * G(wo, wi) * D(h);
+            double down = 4 * dot(Vector3D(0, 0, 1.0), wo) * dot(Vector3D(0, 0, 1.0), wi);
+            ans = up / down;
+            auto f = wavelength_dependent_BSDF(wavelength, ans);
+            ans = f * wavelength / 300;
+        }
+        return ans;
+    }
+
+    Vector3D MicrofacetBSDF::sample_f(const Vector3D wo, Vector3D *wi, double *pdf, double wavelength) {
+        // TODO Assignment 7: Part 2
+        // *Importance* sample Beckmann normal distribution function (NDF) here.
+        // Note: You should fill in the sampled direction *wi and the corresponding *pdf,
+        //       and return the sampled BRDF value.
+
+        //*wi = cosineHemisphereSampler.get_sample(pdf);
+        Vector3D ans(0., 0., 0.);
+        double r1 = sampler.get_sample()[0];
+        double r2 = sampler.get_sample()[1];
+
+        double theta = atan(sqrt(-alpha * alpha * log(1 - r1)));
+        double phi = 2 * PI * r2;
+        Vector3D h(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+
+        *wi = 2. * dot(wo, h) * h - wo;
+        wi->normalize();
+        if (wi->z >= 0) {
+            double p_theta_up = 2 * sin(theta) * exp(-tan(theta) * tan(theta) / (alpha * alpha));
+            double p_theta_down = alpha * alpha * cos(theta) * cos(theta) * cos(theta);
+            double p_theta = p_theta_up / p_theta_down;
+
+            double p_phi = 1. / (2. * PI);
+
+            double pwh = p_theta * p_phi / sin(theta);
+            double pwi = pwh / (4 * dot(*wi, h));
+
+            *pdf = pwi;
+            ans = MicrofacetBSDF::f(wo, *wi, wavelength);
+        }
+        else {
+            *pdf = 0;
+        }
+
+        return ans;
+    }
+
+    void MicrofacetBSDF::render_debugger_node() {
+        if (ImGui::TreeNode(this, "Micofacet BSDF")) {
+            DragDouble3("eta", &eta[0], 0.005);
+            DragDouble3("K", &k[0], 0.005);
+            DragDouble("alpha", &alpha, 0.005);
+            ImGui::TreePop();
+        }
+    }
+
+// Refraction BSDF //
+
+    Vector3D RefractionBSDF::f(const Vector3D wo, const Vector3D wi, double wavelength) {
+        return Vector3D();
+    }
+
+    Vector3D RefractionBSDF::sample_f(const Vector3D wo, Vector3D *wi, double *pdf, double wavelength) {
+        // TODO Assignment 7: Part 1
+        // Implement RefractionBSDF
+
+        *pdf = 1.0;
+        auto t = refract(wo, wi, ior, wavelength);
+        if (!t)
+            return {};
+
+        auto eta = 1.000293 / ior;
+        auto fac = 589.29 / wavelength;
+        eta = eta / fac;
+        if (wo.z < 0)
+            eta = 1.0/eta;
+
+        return transmittance / abs_cos_theta(*wi) / pow(eta, 2.0);
+    }
+
+    void RefractionBSDF::render_debugger_node() {
+        if (ImGui::TreeNode(this, "Refraction BSDF")) {
+            DragDouble3("Transmittance", &transmittance[0], 0.005);
+            DragDouble("ior", &ior, 0.005);
+            ImGui::TreePop();
+        }
+    }
+
+// Glass BSDF //
+
+    Vector3D GlassBSDF::f(const Vector3D wo, const Vector3D wi, double wavelength) {
+        return Vector3D();
+    }
+
+    Vector3D GlassBSDF::sample_f(const Vector3D wo, Vector3D *wi, double *pdf, double wavelength) {
+
+        // TODO Assignment 7: Part 1
+        // Compute Fresnel coefficient and either reflect or refract based on it.
+
+        *pdf = 1.0;
+        auto t = refract(wo, wi, ior, wavelength);
+        if (!t) {
+            reflect(wo, wi);
+            return reflectance / abs_cos_theta(*wi);
+        }
+
+        auto eta = 1.000293 / ior;
+        auto fac = 589.29 / wavelength;
+        eta = eta / fac;
+        if (wo.z < 0)
+            eta = 1.0/eta;
+
+        auto R_0 = pow((1.0 - eta) / (1.0 + eta), 2.0);
+        auto R = R_0 + (1.0 - R_0) * pow(1.0 - abs_cos_theta(wo), 5.0);
+
+        if (coin_flip(R)) {
+            reflect(wo, wi);
+            *pdf = R;
+            return R * reflectance / abs_cos_theta(*wi);
+        }
+        else {
+            *pdf = 1.0 - R;
+            return (1 - R) * transmittance / abs_cos_theta(*wi) / pow(eta, 2.0);
+        }
+    }
+
+    void GlassBSDF::render_debugger_node() {
+        if (ImGui::TreeNode(this, "Refraction BSDF")) {
+            DragDouble3("Reflectance", &reflectance[0], 0.005);
+            DragDouble3("Transmittance", &transmittance[0], 0.005);
+            DragDouble("ior", &ior, 0.005);
+            ImGui::TreePop();
+        }
+    }
+
+    void BSDF::reflect(const Vector3D wo, Vector3D *wi) {
+
+        // TODO Assignment 7: Part 1
+        // Implement reflection of wo about normal (0,0,1) and store result in wi.
+
+        // consider wo already in local coordinates
+        // return wi in local coordinates
+        *wi = Vector3D(-wo.x, -wo.y, wo.z);
+
+    }
+
+    bool BSDF::refract(const Vector3D wo, Vector3D *wi, double ior, double wavelength = 589) {
+
+        // TODO Assignment 7: Part 1
+        // Use Snell's Law to refract wo surface and store result ray in wi.
+        // Return false if refraction does not occur due to total internal reflection
+        // and true otherwise. When dot(wo,n) is positive, then wo corresponds to a
+        // ray entering the surface through vacuum.
+
+        *wi = Vector3D();
+        auto t = dot(wo, Vector3D{0, 0, 1});
+
+        auto eta = 1.000277 / ior;      // standard atmospheric pressure at 0 degree Celsius
+        auto fac = 589.29 / wavelength; // air refractive index at 589.29 nm
+        eta = eta / fac;
+        if (t < 0)  // existing case
+            eta = 1.0 / eta;
+
+        auto internal_term = 1.0 - eta * eta * (1.0 - pow(wo.z, 2.0));
+        if (internal_term < 0)
+            return false;
+
+        wi->x = -eta * wo.x;
+        wi->y = -eta * wo.y;
+        wi->z = sqrt(internal_term);
+
+        if (t > 0)
+            wi->z = -wi->z;
+
+        return true;
+    }
+
+} // namespace CGL
---
+++ src/pathtracer/pathtracer.cpp
@@ -0,0 +1,389 @@
+#include "pathtracer.h"
+
+#include "scene/light.h"
+#include "scene/sphere.h"
+#include "scene/triangle.h"
+#include <random>
+#include <chrono>
+
+#define SAMPLE_PER_COLOR 16
+#define COLOR_TEMPERATURE 40000
+
+using namespace CGL::SceneObjects;
+
+namespace CGL {
+
+    PathTracer::PathTracer() {
+        gridSampler = new UniformGridSampler2D();
+        hemisphereSampler = new UniformHemisphereSampler3D();
+
+        tm_gamma = 2.2f;
+        tm_level = 1.0f;
+        tm_key = 0.18;
+        tm_wht = 5.0f;
+    }
+
+    PathTracer::~PathTracer() {
+        delete gridSampler;
+        delete hemisphereSampler;
+    }
+
+    void PathTracer::set_frame_size(size_t width, size_t height) {
+        sampleBuffer.resize(width, height);
+        sampleCountBuffer.resize(width * height);
+    }
+
+    void PathTracer::clear() {
+        bvh = nullptr;
+        scene = nullptr;
+        camera = nullptr;
+        sampleBuffer.clear();
+        sampleCountBuffer.clear();
+        sampleBuffer.resize(0, 0);
+        sampleCountBuffer.resize(0, 0);
+    }
+
+    void PathTracer::write_to_framebuffer(ImageBuffer &framebuffer, size_t x0,
+                                          size_t y0, size_t x1, size_t y1) {
+        sampleBuffer.toColor(framebuffer, x0, y0, x1, y1);
+    }
+
+    Vector3D
+    PathTracer::estimate_direct_lighting_hemisphere(const Ray &r,
+                                                    const Intersection &isect) {
+        // Estimate the lighting from this intersection coming directly from a light.
+        // For this function, sample uniformly in a hemisphere.
+
+        // Note: When comparing Cornel Box (CBxxx.dae) results to importance sampling, you may find the "glow" around the light source is gone.
+        // This is totally fine: the area lights in importance sampling has directionality, however in hemisphere sampling we don't model this behaviour.
+
+        // make a coordinate system for a hit point
+        // with N aligned with the Z direction.
+        Matrix3x3 o2w;
+        make_coord_space(o2w, isect.n);
+        Matrix3x3 w2o = o2w.T();
+
+        // w_out points towards the source of the ray (e.g.,
+        // toward the camera if this is a primary ray)
+        const Vector3D hit_p = r.o + r.d * isect.t;
+        const Vector3D w_out = w2o * (-r.d);
+
+        // This is the same number of total samples as
+        // estimate_direct_lighting_importance (outside of delta lights). We keep the
+        // same number of samples for clarity of comparison.
+        int num_samples = scene->lights.size() * ns_area_light;
+        Vector3D L_out;
+
+        // TODO (Part 3): Write your sampling loop here
+
+        for (int i = 0; i < num_samples; i++) {
+            auto w_in = hemisphereSampler->get_sample();
+            auto f = isect.bsdf->f(w_out, w_in, r.wavelength) * (2 * PI);
+            auto wi = o2w * w_in;
+            auto nextRay = Ray(hit_p, wi);
+            nextRay.min_t = EPS_F;
+            nextRay.max_t = INF_F - EPS_F;
+            nextRay.color = r.color;
+            nextRay.wavelength = r.wavelength;
+
+            Intersection nextIsect{};
+            if (!bvh->intersect(nextRay, &nextIsect)) continue;
+            L_out += f * nextIsect.bsdf->get_emission() / (double) num_samples;
+        }
+
+        return L_out;
+    }
+
+    Vector3D
+    PathTracer::estimate_direct_lighting_importance(const Ray &r,
+                                                    const Intersection &isect) {
+        // Estimate the lighting from this intersection coming directly from a light.
+        // To implement importance sampling, sample only from lights, not uniformly in
+        // a hemisphere.
+
+        // make a coordinate system for a hit point
+        // with N aligned with the Z direction.
+        Matrix3x3 o2w;
+        make_coord_space(o2w, isect.n);
+        Matrix3x3 w2o = o2w.T();
+
+        // w_out points towards the source of the ray (e.g.,
+        // toward the camera if this is a primary ray)
+        const Vector3D hit_p = r.o + r.d * isect.t;
+        const Vector3D w_out = w2o * (-r.d);
+        Vector3D L_out;
+
+        for (auto light: scene->lights) {
+            if (!light->is_delta_light()) {
+                for (int i = 0; i < ns_area_light; i++) {
+                    Vector3D wi;
+                    double distToLight, pdf;
+
+                    Vector3D lightIntensity = light->sample_L(hit_p, &wi, &distToLight, &pdf);
+
+                    if (pdf == 0) continue;
+
+                    auto f = isect.bsdf->f(w_out, w2o * wi, r.wavelength);
+                    auto nextRay = Ray(hit_p, wi);
+                    nextRay.min_t = EPS_F;
+                    nextRay.max_t = distToLight - EPS_F;
+                    nextRay.color = r.color;
+                    nextRay.wavelength = r.wavelength;
+
+                    if (bvh->has_intersection(nextRay)) continue;
+                    L_out +=
+                            f * lightIntensity / pdf / (double) ns_area_light;
+                }
+            }
+            else {
+                Vector3D wi;
+                double distToLight, pdf;
+
+                Vector3D lightIntensity = light->sample_L(hit_p, &wi, &distToLight, &pdf);
+
+                if (pdf == 0) continue;
+
+                auto f = isect.bsdf->f(w_out, w2o * wi, r.wavelength);
+                auto nextRay = Ray(hit_p, wi);
+                nextRay.min_t = EPS_F;
+                nextRay.max_t = distToLight - EPS_F;
+
+                if (bvh->has_intersection(nextRay)) continue;
+                L_out += f * lightIntensity / pdf;
+            }
+        }
+
+        return L_out;
+    }
+
+    Vector3D PathTracer::zero_bounce_radiance(const Ray &r,
+                                              const Intersection &isect) {
+        // TODO: Part 3, Task 2
+        // Returns the light that results from no bounces of light
+        // (e.g. the light emmitted from a light source)
+
+        return isect.bsdf->get_emission();// * dot(isect.n, r.d.unit())
+
+    }
+
+    Vector3D PathTracer::one_bounce_radiance(const Ray &r,
+                                             const Intersection &isect) {
+        // TODO: Part 3, Task 3
+        // Returns either the direct illumination by hemisphere or importance sampling
+        // depending on `direct_hemisphere_sample`
+
+        if (direct_hemisphere_sample)
+            return estimate_direct_lighting_hemisphere(r, isect);
+        else
+            return estimate_direct_lighting_importance(r, isect);
+    }
+
+    Vector3D PathTracer::at_least_one_bounce_radiance(const Ray &r,
+                                                      const Intersection &isect) {
+        Matrix3x3 o2w;
+        make_coord_space(o2w, isect.n);
+        Matrix3x3 w2o = o2w.T();
+
+        Vector3D hit_p = r.o + r.d * isect.t;
+        Vector3D w_out = w2o * (-r.d);
+
+        Vector3D L_out(0, 0, 0);
+
+        // TODO: Part 4, Task 2
+        // Returns the one bounce radiance + radiance from extra bounces at this point.
+        // Should be called recursively to simulate extra bounces.
+        if (!isect.bsdf->is_delta())
+            L_out += one_bounce_radiance(r, isect);
+        // enable it if indirect only
+        // if (r.depth == max_ray_depth) L_out = Vector3D(0, 0, 0);
+        if (r.depth == 0) return L_out;
+
+
+        Vector3D w_in;
+        double pdf;
+
+        auto f = isect.bsdf->sample_f(w_out, &w_in, &pdf, r.wavelength);
+        auto wi = o2w * w_in;
+        auto ray = Ray(hit_p, wi);
+        ray.depth = r.depth - 1;
+        ray.min_t = EPS_F;
+        ray.max_t = INF_D - EPS_F;
+        ray.color = r.color;
+        ray.wavelength = r.wavelength;
+
+        Intersection shadowIsect;
+        if (bvh->intersect(ray, &shadowIsect) && coin_flip(0.7)) {
+            auto L = at_least_one_bounce_radiance(ray, shadowIsect);
+            if (isect.bsdf->is_delta())
+                L += zero_bounce_radiance(ray, shadowIsect);
+            auto f_l = f * L;
+            L_out += f_l * abs_cos_theta(w_in) / pdf / 0.7;
+        }
+
+        return L_out;
+    }
+
+    Vector3D PathTracer::est_radiance_global_illumination(const Ray &r) {
+        Intersection isect;
+        Vector3D L_out;
+
+        // You will extend this in assignment 3-2.
+        // If no intersection occurs, we simply return black.
+        // This changes if you implement hemispherical lighting for extra credit.
+
+        // The following line of code returns a debug color depending
+        // on whether ray intersection with triangles or spheres has
+        // been implemented.
+        //
+        // REMOVE THIS LINE when you are ready to begin Part 3.
+
+        if (!bvh->intersect(r, &isect))
+            return envLight ? envLight->sample_dir(r) : L_out;
+
+        // L_out = (isect.t == INF_D) ? debug_shading(r.d) : normal_shading(isect.n);
+
+        // TODO (Part 3): Return the direct illumination.
+        L_out = zero_bounce_radiance(r, isect);
+        // L_out += one_bounce_radiance(r, isect);
+
+        // TODO (Part 4): Accumulate the "direct" and "indirect"
+        // parts of global illumination into L_out rather than just direct
+        L_out += at_least_one_bounce_radiance(r, isect);
+
+        return L_out;
+    }
+
+    void PathTracer::raytrace_pixel(size_t x, size_t y) {
+        // TODO (Part 1.2):
+        // Make a loop that generates num_samples camera rays and traces them
+        // through the scene. Return the average Vector3D.
+        // You should call est_radiance_global_illumination in this function.
+
+        // TODO (Part 5):
+        // Modify your implementation to include adaptive sampling.
+        // Use the command line parameters "samplesPerBatch" and "maxTolerance"
+
+//        int num_samples = ns_aa;          // total samples to evaluate
+        Vector2D origin = Vector2D(x, y); // bottom left corner of the pixel
+
+        // My code Start
+
+        Vector3D radiance(0.0);
+
+        size_t num_samples = 0;
+
+        double s1 = 0, s2 = 0, miu, sigma;
+
+        do {
+            auto rayList = std::list<Ray>();
+
+            auto sample = origin + gridSampler->get_sample();
+
+            // take a few samples for each color channel
+            for (int i = 0; i < SAMPLE_PER_COLOR * 3; i++) {
+                auto r = camera->generate_ray(sample.x / sampleBuffer.w, sample.y / sampleBuffer.h, i % 3);
+                r.depth = max_ray_depth;
+                rayList.push_back(r);
+            }
+
+            // combine three channels and divide by the number of channels
+            auto newRadiance = Vector3D();
+            for (const auto &r: rayList) {
+                auto tmp = est_radiance_global_illumination(r);
+                newRadiance[r.color] += tmp[r.color];
+            }
+            newRadiance /= (rayList.size() / 3);
+
+            radiance = (radiance * num_samples + newRadiance) / (num_samples + 1);
+
+            num_samples++;
+
+            s1 += newRadiance.illum();
+            s2 += newRadiance.illum() * newRadiance.illum();
+
+            if (num_samples % samplesPerBatch == 0) {
+                miu = s1 / num_samples;
+                sigma = sqrt((s2 - s1 / num_samples * s1) / (num_samples - 1));
+
+                if (1.96 * sigma < maxTolerance * miu * sqrt(num_samples))
+                    break;
+            }
+
+        } while (num_samples < ns_aa);
+
+        auto temperature = COLOR_TEMPERATURE;
+        for (int color = 0; color < 3; color++) {
+            auto radiance_cof = color_temperature(temperature, color);
+            radiance_cof /= 255;
+            radiance[color] *= radiance_cof;
+        }
+
+        sampleBuffer.update_pixel(radiance, x, y);
+        sampleCountBuffer[x + y * sampleBuffer.w] = num_samples;
+
+        // My code End
+
+
+//        sampleBuffer.update_pixel(Vector3D(0.2, 1.0, 0.8), x, y);
+//        sampleCountBuffer[x + y * sampleBuffer.w] = num_samples;
+
+    }
+
+    void PathTracer::autofocus(Vector2D loc) {
+        Ray r = camera->generate_ray(loc.x / sampleBuffer.w, loc.y / sampleBuffer.h, 0);
+        Intersection isect;
+
+        bvh->intersect(r, &isect);
+
+        camera->focalDistance = isect.t;
+    }
+
+    // color temperature range from 1000K to 40000K
+    double PathTracer::color_temperature(int temperature, int color) {
+        double radiance;
+        temperature /= 100;
+        switch (color)
+        {
+            case 0: // R
+                if (temperature <= 66) {
+                    radiance = 255;
+                } else {
+                    radiance = temperature - 60;
+                    radiance = 329.698727446 * (pow(radiance, -0.1332047592));
+                    if (radiance < 0) radiance = 0;
+                    if (radiance > 255) radiance = 255;
+                }
+                break;
+
+            case 1: // G
+                if (temperature <= 66) {
+                    radiance = temperature;
+                    radiance = 99.4708025861 * log(radiance) - 161.1195681661;
+                    if (radiance < 0) radiance = 0;
+                    if (radiance > 255) radiance = 255;
+                } else {
+                    radiance = temperature - 60;
+                    radiance = 288.1221695283 * pow(radiance, -0.0755148492);
+                    if (radiance < 0) radiance = 0;
+                    if (radiance > 255) radiance = 255;
+                }
+                break;
+
+            case 2: // B
+                if (temperature >= 66) {
+                    radiance = 255;
+                } else {
+                    if (temperature <= 19) {
+                        radiance = 0;
+                    } else {
+                        radiance = temperature - 10;
+                        radiance = 138.5177312231 * log(radiance) - 305.0447927307;
+                        if (radiance < 0) radiance = 0;
+                        if (radiance > 255) radiance = 255;
+                    }
+                }
+        }
+        return radiance;
+    }
+
+} // namespace CGL
